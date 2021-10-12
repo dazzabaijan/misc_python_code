45f9314c678fa9c18f1345570072f49e1cd6d404
Darren Ng <darren.ng@versed.ai>
2021-10-12 23:56:49 +0100

    binary search for a sorted list added doc

--- binary_search_sorted_list.py
+++ binary_search_sorted_list.py
@@ -1,6 +1,14 @@
 """Binary search in a sorted list"""
 
 def binary_search(array, value):
+    """
+    At each step, we reduce our search space by half.
+    Compare middle value with search value.
+    If search value < mid value, we search left side
+    If search value > mid value, we search right side
+    If we find search value, return index or return
+    false otherwise.
+    """
     size = len(array)
     low = 0
     high = size - 1
@@ -15,4 +23,5 @@ def binary_search(array, value):
                 high = mid - 1
     return False
 
-print(binary_search([1,3,5,6,7], 5))
+if __name__ == "__main__":
+    print(binary_search([1,3,5,6,7], 5))
