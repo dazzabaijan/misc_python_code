b7493c301d5f691fc025b980f390832bc13bfa18
Darren Ng <darren.ng@versed.ai>
2021-12-10 21:30:24 +0000

    clean up

--- cython_basic/setup_example_pyx.py
+++ cython_basic/setup_example_pyx.py
@@ -2,4 +2,4 @@ from Cython.Build.Dependencies import cythonize
 from distutils.core import setup
 from Cython.Build import Cythonize
 
-setup(ext_modules=cythonize('example.pyx'))
+setup(ext_modules=cythonize('example_cy.pyx'))
